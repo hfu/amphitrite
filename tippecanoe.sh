04b67598b2f6d54b2db4c7affdf0f619a2e24246
Hidenori Fujimura <hfu@users.noreply.github.com>
2018-02-28 23:02:25 +0000

    update

--- tippecanoe.sh
+++ tippecanoe.sh
@@ -1,2 +1,2 @@
-../tippecanoe/tippecanoe --read-parallel --simplification=2 --minimum-zoom=10 --maximum-zoom=15 --base-zoom=15 -f --output=/export/amphitrite.mbtiles amphitrite.ndjson
+../tippecanoe/tippecanoe --read-parallel --simplify-only-low-zooms --simplification=4 --minimum-zoom=10 --maximum-zoom=16 --base-zoom=16 -f --output=/export/amphitrite.mbtiles amphitrite.ndjson
 
