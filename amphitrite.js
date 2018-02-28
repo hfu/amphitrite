a11565680bc92568f10332e92b7e95b79ea3ca00
Hidenori Fujimura <hfu@users.noreply.github.com>
2018-02-28 22:28:12 +0000

    updatE

--- amphitrite.js
+++ amphitrite.js
@@ -2,68 +2,68 @@ const Database = require('better-sqlite3')
 const VectorTile = require('@mapbox/vector-tile').VectorTile
 const Protobuf = require('pbf')
 const zlib = require('zlib')
-const lut = {
-  'Cstline': f => {
-    return {layer: 'cstline', minzoom: 10, maxzoom: 15}
-  },
-  'Cntr': f => {
-    return {
-      layer: 'cntr', 
-      minzoom: f.properties.alti % 100 === 0 ? 10 : 
-        (f.properties.alti % 50 === 0 ? 12 : 14),
-      maxzoom: 15
-    }
-  },
-  'RdEdg': f => {
-    return {layer: 'rdedg', minzoom: 14, maxzoom: 15}
-  },
-  'ElevPt': f => {
-    return {layer: 'elevpt', minzoom: 14, maxzoom: 15}
-  },
-  'BldL': f => {
-    return {layer: 'bldl', minzoom: 15, maxzoom: 15}
-  },
-  'GCP': f => {
-    return {layer: 'gcp', minzoom: 15, maxzoom: 15}
-  },
-  'WL': f => {
-    return {layer: 'wl', minzoom: 14, maxzoom: 15}
-  },
-  'AdmPt': f => {
-    return {layer: 'admpt', minzoom: 15, maxzoom: 15}
-  },
-  'RdCompt': f => {
-    return {layer: 'rdcompt', minzoom: 15, maxzoom: 15}
-  },
-  'CommBdry': f => {
-    return {layer: 'commbdry', minzoom: 14, maxzoom: 15}
-  },
-  'AdmBdry': f => {
-    return {layer: 'admbdry', minzoom: 12, maxzoom: 15}
-  },
-  'WStrL': f => {
-    return {layer: 'wstrl', minzoom: 14, maxzoom: 15}
-  },
-  'CommPt': f => {
-    return {layer: 'commpt', minzoom: 14, maxzoom: 15}
-  },
-  'RailCL': f => {
-    return {layer: 'railcl', minzoom: 10, maxzoom: 15}
-  },
-  'SBBdry': f => {
-    return {layer: 'sbbdry', minzoom: 14, maxzoom: 15}
-  },
-  'SBAPt': f => {
-    return {layer: 'sbapt', minzoom: 14, maxzoom: 15}
-  }
-}
-
 const params = {
   src: '/export/experimental_fgd.mbtiles', Z: 18,
   z: 10, x: 881, y: 413
 }
 
-const db = new Database(params.src)
+const lut = {
+  'Cstline': f => {
+    return {layer: 'cstline', minzoom: 10, maxzoom: 16}
+  },
+  'Cntr': f => {
+    return {
+      layer: 'cntr', 
+      minzoom: f.properties.alti % 100 === 0 ? 10 : 
+        (f.properties.alti % 50 === 0 ? 13 : 15),
+      maxzoom: 16
+    }
+  },
+  'RdEdg': f => {
+    return {layer: 'rdedg', minzoom: 14, maxzoom: 16}
+  },
+  'ElevPt': f => {
+    return {layer: 'elevpt', minzoom: 15, maxzoom: 16}
+  },
+  'BldL': f => {
+    return {layer: 'bldl', minzoom: 16, maxzoom: 16}
+  },
+  'GCP': f => {
+    return {layer: 'gcp', minzoom: 16, maxzoom: 16}
+  },
+  'WL': f => {
+    return {layer: 'wl', minzoom: 14, maxzoom: 16}
+  },
+  'AdmPt': f => {
+    return {layer: 'admpt', minzoom: 16, maxzoom: 16}
+  },
+  'RdCompt': f => {
+    return {layer: 'rdcompt', minzoom: 16, maxzoom: 16}
+  },
+  'CommBdry': f => {
+    return {layer: 'commbdry', minzoom: 15, maxzoom: 16}
+  },
+  'AdmBdry': f => {
+    return {layer: 'admbdry', minzoom: 13, maxzoom: 16}
+  },
+  'WStrL': f => {
+    return {layer: 'wstrl', minzoom: 14, maxzoom: 16}
+  },
+  'CommPt': f => {
+    return {layer: 'commpt', minzoom: 15, maxzoom: 16}
+  },
+  'RailCL': f => {
+    return {layer: 'railcl', minzoom: 13, maxzoom: 16}
+  },
+  'SBBdry': f => {
+    return {layer: 'sbbdry', minzoom: 15, maxzoom: 16}
+  },
+  'SBAPt': f => {
+    return {layer: 'sbapt', minzoom: 16, maxzoom: 16}
+  }
+}
+
+const db = new Database(params.src, {readonly: true})
 const dz = params.Z - params.z
 const stmt = db.prepare(
   'SELECT * FROM tiles WHERE tile_row = ? AND tile_column = ? AND zoom_level = ?'
